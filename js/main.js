df610343c9a82e09566d062795d6a68198ea7596
Aarti <aarti@support.unlocktech.in>
2025-01-31 17:51:52 -0600

    commit about and services section

--- js/main.js
+++ js/main.js
@@ -7,4 +7,32 @@ $(document).ready(function(){
         autoplay:true
 
     });
+    $('.serviceslist').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay:true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              }
+        ]
+
+    });
   });
