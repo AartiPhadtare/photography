75d7977151787b92e989a8cfc675733c65fce163
Aarti <aarti@support.unlocktech.in>
2025-02-06 16:58:45 -0600

    commit - portfolio section & footer

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     });
     $('.serviceslist').slick({
         slidesToShow: 4,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         arrows: false,
         dots: true,
         autoplay:true,
@@ -35,4 +35,41 @@ $(document).ready(function(){
         ]
 
     });
+
+    var $grid = $('.grid').isotope({
+      itemSelector: '.element-item',
+      getSortData: {
+       
+        category: '[data-category]',
+        
+      }
+    });
+         // filter functions
+         var filterFns = {
+        };
+    
+        // bind filter button click
+        $('#filters').on( 'click', 'button', function() {
+          var filterValue = $( this ).attr('data-filter');
+          // use filterFn if matches value
+          filterValue = filterFns[ filterValue ] || filterValue;
+          $grid.isotope({ filter: filterValue });
+        });
+    
+        $('.button-group').each( function( i, buttonGroup ) {
+          var $buttonGroup = $( buttonGroup );
+          $buttonGroup.on( 'click', 'button', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $( this ).addClass('is-checked');
+          });
+        });  
+  
+  
   });
+
+  if ($('.wow').length) {
+    new WOW().init();
+}
+
+ 
+
